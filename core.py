25f172f3a396834d6e501f3f4fbdf5b212063ba3
Kristen Pekala <krispekala@tamu.edu>
2017-11-13 19:34:33 -0600

    separation
    separated women from men.

--- core.py
+++ core.py
@@ -1,7 +1,24 @@
 import json
+import gender_guesser.detector as gender
+
+# def initial_rank():
+#     pass
+
 
 with open('users.dat') as data:
+    d = gender.Detector()
+    usrs = {}
     for line in data: 
         users = json.loads(line)
-        print(users['login'])
+        name = users['name']
+        if type(name) is str: 
+            firstname = name.split()[0]
+            sex = d.get_gender(firstname)
+            #print(name, sex)
+            if sex != 'female' and sex != 'mostly_female':
+                usrs[users['name']] = 1
+                
+    print(usrs)
+        
+
         
